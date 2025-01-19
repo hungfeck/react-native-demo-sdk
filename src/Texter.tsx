c1330aa5cb687fb7d8539fad39ef69ca97dc3ea2
hungfeck <hungfeck@gmail.com>
2025-01-19 09:53:29 +0700

    update Texter

--- src/Texter.tsx
+++ src/Texter.tsx
@@ -1,17 +1,12 @@
-import * as React from 'react';
-import { Button, NativeModules, StyleSheet, Text, useColorScheme, View } from 'react-native';
-
-interface Props {
-}
+import { StyleSheet, Text, View } from 'react-native';
 
 
 
-export const TexTer: React.FC<React.PropsWithChildren<Props>> = ({ children }) => {
-  const [count, setCount] = React.useState(0)
+export const TexTer = () => {
 
   return (
     <View style={styles.container}>
-      <Text>You pressed {count} times</Text>
+      <Text>You pressed 3 times</Text>
     </View>
   )
 }
