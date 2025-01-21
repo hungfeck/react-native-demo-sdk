af9bfbbca930295e41e56d9f3f58868ff2678d93
hungfeck <hungfeck@gmail.com>
2025-01-21 17:05:03 +0700

    thêm react-native.config

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    'react-native-webview': {
+      platforms: {
+        ios: {
+          podspecPath: './node_modules/react-native-webview/react-native-webview.podspec',
+        },
+      },
+    },
+  },
+};
